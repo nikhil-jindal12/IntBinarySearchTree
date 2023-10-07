20239e2faafb774f1d0352736f07764561b69887
Nikhil Jindal <njindal2004@gmail.com>
2023-10-07 17:00:39 -0400

    Added comments and fixed methods

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,24 +1,49 @@
-public class BinarySearchTree {
-    private Node root;
-    private static int nodeCount;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Class and relevant methods for a Binary Search Tree (BST) that contains integer keys
+ * @author Nikhil Jindal
+ */
+public class BinarySearchTree {
+
+    // instance field that stores the root of the BST
+    private Node root;
+
+    // instance field that counts the number of nodes in the BST as they are inserted
+    private int nodeCount;
+
+    // Constructor for the BST class
     public BinarySearchTree() {
         this.root = null;
     }
 
+    /**
+     * Returns the root of the BST
+     * @return the root of the BST
+     */
     public Node getRoot() {
         return this.root;
     }
 
-    public void setRoot(Node root) {
-        this.root = root;
+    /**
+     * Returns the number of nodes in the BST
+     * @return
+     */
+    public int getNodeCount() {
+        return this.nodeCount;
     }
 
-    public static void insert(Node root, int value) {
+    /**
+     * Inserts a node into the BST based on the value given
+     * @param root  the root of the BST
+     * @param value  the key which determines where the node will be placed into the BST
+     */
+    public void insert(Node root, int value) {
         Node parent = null;
         Node trav = root;
-        nodeCount++;
-
+        
+        // traverse the BST until a leaf node is reached
         while (trav != null) {
             parent = trav;
             if (value < trav.getKey()) {
@@ -27,7 +52,8 @@ public class BinarySearchTree {
                 trav = trav.getRight();
             }
         }
-
+        
+        // insert the node into the BST based on the value of its key and in correspondence with its parent node
         if (parent == null) {
             root = new Node(value);
         } else if (value < parent.getKey()) {
@@ -35,81 +61,103 @@ public class BinarySearchTree {
         } else {
             parent.setRight(new Node(value));
         } 
-    
+        
+        // increment the node count
+        nodeCount++;
+
     }
 
-    public static void preorderRec(Node root) {
+    /**
+     * Recursive method that prints the preorder traversal of the BST
+     * @param root  the root of the BST
+     */
+    public void preorderRec(Node root) {
+        
+        // base case
         if (root == null) {
             return;
         }
-        System.out.print(root.getKey() + " ");
+
+        // recursive case:
+
+        // traverses the left subtree of a node
         preorderRec(root.getLeft());
 
+        // traverses the right subtree of a node
         preorderRec(root.getRight());
 
     }
 
+    /**
+     * Recursive method that returns the sum of all the keys in the BST
+     * @param root  the root of the BST
+     * @return  the sum of all the keys in the BST
+     */
     public int sum(Node root) {
+        
         if (root == null) {
+
+            // base case
             return 0;
+
         } else {
+
+            // recursive case: 
+            // adds the current node's key to the sum of the keys in the left and right subtrees
             return root.getKey() + sum(root.getLeft()) + sum(root.getRight());
+
         }
+
     }
 
-    // finds the Node that is the kth biggest Node of the entire BinarySearchTree
-    public static Node kthBiggest(Node root, int k) throws IndexOutOfBoundsException {        
-        if (k > nodeCount) {
+    /**
+     * Method that returns the kth biggest element in the BST
+     * @param root  the root of the BST
+     * @param k  the kth largest element in the BST
+     * @return  the kth largest element in the BST
+     * @throws IndexOutOfBoundsException  if k is greater than the number of nodes in the BST
+     */
+    public Node kthBiggest(Node root, int k) throws IndexOutOfBoundsException {        
+        
+        // check to see if the node requested exists in the BST
+        if (k > getNodeCount() + 1) {
             throw new IndexOutOfBoundsException();
         }
 
-        return reverseInorderRec(root, k);
+        // create a List that will store all of the Nodes in the BST
+        List<Node> elements = new ArrayList<>();
+
+        // perform an inorder traversal of the BST
+        inorderRec(root, elements);
+
+        // return the kth largest element in the List
+        return elements.get(getNodeCount() - k + 1);
 
     }
 
-    private static Node reverseInorderRec(Node root, int k) {
-        if (root == null || k == 0) {
-            return null;
-        }
+    /**
+     * Recursive method that performs an inorder traversal of the BST and stores the Nodes in a List
+     * @param root  the root of the BST
+     * @param elements  the List that will store the Nodes in the BST
+     */
+    private void inorderRec(Node root, List<Node> elements) {
         
-        while (k > 0) {
-            // Recursively traverse the right subtree first
-            reverseInorderRec(root.getRight(), k);
-            if (root.getRight() != null) {
-                k--;
-            }
-            
-            
-            // Continue with the left subtree
-            reverseInorderRec(root.getLeft(), k);
-            if (root.getLeft() != null) {
-                k--;
-            }
-            
-            return root;
-            
+        // base case
+        if (root == null) {
+            return;
         }
 
-        return root;
+        // recursive case:
 
-    }
-    
+        // traverses the left subtree of a node
+        inorderRec(root.getLeft(), elements);
+        
+        // adds the current node to the List
+        elements.add(root);
 
-    public static void main(String[] args) {
-        Node root = new Node(5);
-        insert(root, 3);
-        insert(root, 7);
-        insert(root, 2);
-        insert(root, 4);
-        insert(root, 6);
-        insert(root, 8);
+        // traverses the right subtree of a node
+        inorderRec(root.getRight(), elements);
 
-
-        // System.out.println(root.getLeft().getRight().getRight().getRight().getKey());
-        System.out.println(root.getRight().getLeft().getKey());
-        System.out.println(kthBiggest(root, 3).getKey());
-        // preorderRec(root);
-    }
-    
+    }    
 
 }
--- Node.java
+++ Node.java
@@ -1,32 +1,58 @@
+/**
+ * Class that contains the implementation for a node of a binary search tree that holds integers
+ */
 public class Node {
+
+    // instance field that holds the key of the node
     private int key;
+
+    // instance field that holds the left child of the node
     private Node left;
+
+    // instance field that holds the right child of the node
     private Node right;
 
+    // constructor that initializes the key of the node
     public Node(int key) {
         this.key = key;
     }
 
+    /**
+     * Returns the key of the node
+     * @return the key of the node
+     */
     public int getKey() {
         return this.key;
     }
 
-    public void setKey(int key) {
-        this.key = key;
-    }
-
+    /**
+     * Returns the left child of the node
+     * @return the left child of the node
+     */
     public Node getLeft() {
         return this.left;
     }
 
+    /**
+     * Sets the left child of the node
+     * @param left  the left child of the node
+     */
     public void setLeft(Node left) {
         this.left = left;
     }
 
+    /**
+     * Returns the right child of the node
+     * @return the right child of the node
+     */
     public Node getRight() {
         return this.right;
     }
 
+    /**
+     * Sets the right child of the node
+     * @param right  the right child of the node
+     */
     public void setRight(Node right) {
         this.right = right;
     }
